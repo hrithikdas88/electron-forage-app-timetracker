efa75bd591b9566648e9dbc8db5d0a5d215a1b23
Hrithik <hrithik,d@webandcrafts.comgit config --global user.email hrithik.d@webandcrafts.com>
2024-03-04 16:06:13 +0530

    sfdsfd

--- src/Entry.jsx
+++ src/Entry.jsx
@@ -1,51 +1,21 @@
 import React, { useEffect, useState } from "react";
 import ProjectList from "./Components/ProjectList/ProjectList.jsx";
-import nookies from 'nookies';
 
 const Entry = () => {
   const [data, setData] = useState("");
   const [currentProject, setCurrentProject] = useState(null);
   const [timers, setTimers] = useState({});
 
-  const handleTimer = (projectName) => {
-    // Stop the previous timer before starting a new one
-    stopTimer();
-    
-    // Start the new timer
-    const startTime = Date.now();
-    setCurrentProject(projectName);
-    setTimers(prevTimers => ({
-      ...prevTimers,
-      [projectName]: startTime,
-    }));
-  };
-
-  const stopTimer = () => {
-    if (currentProject && timers[currentProject]) {
-      const elapsed = Date.now() - timers[currentProject];
-      setTimers(prevTimers => ({
-        ...prevTimers,
-        [currentProject]: undefined,
-      }));
-      console.log(`Timer stopped for ${currentProject}. Elapsed time: ${elapsed} milliseconds.`);
-    }
-  };
-
   useEffect(() => {
     window.indexBridge.authSucess((event, receivedData) => {
       if (receivedData.commandLine[2] === "electron://open") {
         setData(receivedData);
-        nookies.set(receivedData.commandLine[2], {
-          maxAge: 30 * 24 * 60 * 60,
-          path: '/',
-        });
+        // nookies.set(receivedData.commandLine[2], {
+        //   maxAge: 30 * 24 * 60 * 60,
+        //   path: "/",
+        // });
       }
     });
-
-    return () => {
-      // Cleanup: Stop the timer when the component unmounts
-      stopTimer();
-    };
   }, [currentProject, timers]);
 
   const ss = async () => {
@@ -62,18 +32,7 @@ const Entry = () => {
   return (
     <div>
       <button onClick={ipcHandle}>click me</button>
-      {!data ? `login to see your projects` : (
-        <div>
-          <h2>Project List</h2>
-          <ul>
-            {projects.map((project) => (
-              <li key={project.id} onClick={() => handleTimer(project.name)}>
-                {project.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {!data ? `login to see your projects` : <ProjectList />}
       <p>
         Then: Launch the app from a web link!
         <a href="electron://open">Click here to launch the app</a>
